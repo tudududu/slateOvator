6aaa88e9baf0b55d090a853d70622be06396ef88
JanSvatuska <jan.svatuska@email.cz>
2026-02-05 13:22:27 +0100

    slateOvator1 change media, sound, operator field

--- js_slateOvator1.jsx
+++ js_slateOvator1.jsx
@@ -1,6 +1,6 @@
 //  slateOvator_part1
-//  v04
-//  change operator, media, sound field in multiple slates
+//  v05
+//  change media, sound, operator field in multiple slates
 
 (function (thisObj) {
     
@@ -9,11 +9,23 @@
     function newPanel(thisObj) {
         var win = (thisObj instanceof Panel) ? thisObj 
         : new Window('palette', 'slateOvator', undefined);
+        win.orientation = 'column';
         //  win.preferredSize = [350, 300];
+        var buttonSize = [50, 25];
 
         var groupOne = win.add('group');
             groupOne.orientation = 'column';
             groupOne.alignChildren = 'fill';
+        var panelOne = groupOne.add('panel', undefined, 'Fields');
+            panelOne.orientation = 'column';
+            panelOne.alignChildren = 'right';
+        var panelOneGroupOne = panelOne.add('group', undefined, 'panelOneGroupOne');
+            panelOneGroupOne.orientation = 'row';
+        var panelOneGroupTwo = panelOne.add('group', undefined, 'panelOneGroupTwo');
+            panelOneGroupTwo.orientation = 'row';
+        var panelOneGroupThree = panelOne.add('group', undefined, 'panelOneGroupThree');
+            panelOneGroupThree.orientation = 'row';
+        
         var groupTwo = win.add('group');
             groupTwo.orientation = 'column';
             groupTwo.alignChildren = 'fill';
@@ -22,30 +34,31 @@
             groupThree.alignChildren = 'fill';
         
         //  label
-        var labelOne = groupOne.add('statictext', undefined, 'Media: ');
-        var labelTwo = groupTwo.add('statictext', undefined, 'SoundLevel: ');
-        var labelThree = groupThree.add('statictext', undefined, 'Operator: ');
+        var labelOne = panelOneGroupOne.add('statictext', undefined, 'Media: ');
+        var labelTwo = panelOneGroupTwo.add('statictext', undefined, 'SoundLevel: ');
+        var labelThree = panelOneGroupThree.add('statictext', undefined, 'Operator: ');
         //  input text
-        var inputMedia = groupOne.add('edittext', undefined, 'TV');
+        var inputMedia = panelOneGroupOne.add('edittext', undefined, 'TV');
             inputMedia.characters = 10;
-        var inputSoundLevel = groupOne.add('edittext', undefined, 'soundLevel');
+        var inputSoundLevel = panelOneGroupTwo.add('edittext', undefined, 'soundLevel');
             inputSoundLevel.characters = 10;
-        var inputOperator = groupOne.add('edittext', undefined, 'yourName');
+        var inputOperator = panelOneGroupThree.add('edittext', undefined, 'yourName');
             inputOperator.characters = 10;
         //  apply Button
-        var applyBtnOne = groupOne.add('button', undefined, 'Apply');
-        var applyBtnTwo = groupOne.add('button', undefined, 'Apply');
-        var applyBtnThree = groupOne.add('button', undefined, 'Apply');
-        //  applyBtn.size = [50, 25];
+        var buttonOne = panelOneGroupOne.add('button', undefined, 'OK');
+            //buttonOne.size = buttonSize;
+        var buttonTwo = panelOneGroupTwo.add('button', undefined, 'OK');
+        var buttonThree = panelOneGroupThree.add('button', undefined, 'OK');
+        
 
         // --- Action ---
-        applyBtnOne.onClick = function () {
+        buttonOne.onClick = function () {
         slateOvator1('Media', inputMedia.text);
         }
-        applyBtnTwo.onClick = function () {
+        buttonTwo.onClick = function () {
         slateOvator1('SoundLevel', inputSoundLevel.text);
         }
-        applyBtnTwo.onClick = function () {
+        buttonThree.onClick = function () {
         slateOvator1('Operator', inputOperator.text);
         }
 
@@ -58,7 +71,7 @@
 
     }
 
-function slateOvator1(layer, newTextInput) {
+function slateOvator1(layerName, newTextInput) {
 
 app.beginUndoGroup("slateOvator1");
     var selectedComp = app.project.selection; //array
@@ -66,17 +79,17 @@ app.beginUndoGroup("slateOvator1");
     if (selectedComp.length == 0) {
         alert("Select a composition");
     } else {
-        slateOvatorEngine(selectedComp, newText);
+        slateOvatorEngine(selectedComp, layerName, newTextInput);
     }
     
 app.endUndoGroup();
     
-    function slateOvatorEngine(comp, layer, newText) {
+    function slateOvatorEngine(comp, layerName, newTextInput) {
         for (var j = 0; j < comp.length; j++) {
             var layerArr = comp[j].layers;
             for (var i = 1; i <= layerArr.length; i++) {
-                if (layerArr[i].name == layer) {
-                    layerArr[i].text.sourceText.setValue(newText);
+                if (layerArr[i].name == layerName) {
+                    layerArr[i].text.sourceText.setValue(newTextInput);
                 }
             }
         }

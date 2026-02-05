98627b4152bfed6ca70cd11eedd11695cb93c1fc
JanSvatuska <jan.svatuska@email.cz>
2026-02-05 23:44:58 +0100

    slate0vator4 v01 duplicator_inRoot 05a

--- dev/s04_completeAuto/duplicator_v01_inRoot/slate0vator4_v01.jsx
+++ dev/s04_completeAuto/duplicator_v01_inRoot/slate0vator4_v01.jsx
@@ -1,18 +1,16 @@
 // slate0vator4 v01 duplicator_inRoot
-// 240114
+// 240127
 
 //  The FinalCountdown
-//  240114_04a
-//  out0vator
+//  240119_05a
+//  out0vator prebal0vator
 
-
-//-----------------------------------
 
 function slateOvator_part04() {
 app.beginUndoGroup("Prebal0vator");
     var selected = app.project.selection;
     //var regex = slateRegex();
-    var outFolderName = "compsOut";
+    var outFolderName = "outComps";
     var masterFolderName = "masters";
 
     if (selected.length == 0) {
@@ -21,9 +19,6 @@ app.beginUndoGroup("Prebal0vator");
         prebalovator(selected, outFolderName, masterFolderName);
     }
 app.endUndoGroup();
-
-//var projItems = app.project.items;
-//var myCompMaster = app.project.activeItem;
 
 function prebalovator(compSelection, folderName1, folderName2) {
         for (var j = 0; j < compSelection.length; j++) {
@@ -36,21 +31,24 @@ function prebalovator(compSelection, folderName1, folderName2) {
 //criar "out" folder se ja nao existe
     function makeFolder(folderName) {
         //  scan proj if folder exist
+        //  pushes 'true' into testArr in case the folder exists
         var testArr = [];
         for (var i = 1; i <= app.project.numItems; i++){
-            if (app.project.item(i) instanceof FolderItem && app.project.item(i).name == folderName) {
+            if (app.project.item(i).name == folderName 
+                && app.project.item(i) instanceof FolderItem) {
                 testArr.push(true);
             }
         }
-            //alert("numero dos outF: " + testArr.length);
+        //  if the folder not exist
+        //  create a new FolderItem in project
             var folderObj;
             if (testArr.length == 0) {
-            //create a new FolderItem in project, with name VAR
                 app.project.items.addFolder(folderName);
-            }
-            //find folderObj in the proj and put it to var
+                }
+        //  find folderObj in the proj and put it to var
             for (var i = 1; i <= app.project.numItems; i++){
-            if (app.project.item(i) instanceof FolderItem && app.project.item(i).name == folderName) {
+            if (app.project.item(i).name == folderName 
+                && app.project.item(i) instanceof FolderItem) {
                 folderObj = app.project.item(i);
             }
         }
@@ -58,13 +56,15 @@ function prebalovator(compSelection, folderName1, folderName2) {
         }
 
     function finalCountdown(myCompMaster, folderName1, folderName2) {
-        var myCompMasterDur = myCompMaster.duration;
         
-        //alert('myCompMasterNameHolder: ' + myCompMasterNameHolder);
+        //  a criar o comp para o export
+        var myCompMasterDur = myCompMaster.duration;
         var myCompOut = myCompMaster.duplicate();
             myCompOut.duration = myCompMasterDur + 1;
             myCompOut.displayStartTime = -1;
-        //  naming
+        //  naming: 
+        //  passar o nome original para o compOut
+        //  usar um arr para o troco
         var compNameArr = [];
         compNameArr.push(myCompMaster.name);
         compNameArr.push(myCompOut.name);
@@ -72,18 +72,18 @@ function prebalovator(compSelection, folderName1, folderName2) {
         myCompMaster.name = compNameArr[0] + masterAppendix;
         myCompOut.name = compNameArr[0];
         
-        //  delete layer in myCompOut
+        //  delete layers in myCompOut
         var myCompOutLayers = myCompOut.layers;
         for (var i = myCompOutLayers.length; i >= 1; i--) {
             var curLayer = myCompOutLayers[i];
             curLayer.remove();
         }
-
+        //  passar o master pro outComp
             myCompOutLayers.add(myCompMaster);
         var myCompMasterLayerStartTime = myCompOut.layer(1).startTime;
             myCompOut.layer(1).startTime = 1;
-    //move myCompOut into new folder by setting
-    //comp Item's parentFolder to "compsOut" folder
+        //  move myCompOut into new folder by setting
+        //  comp Item's parentFolder to "compsOut" folder
             var myOutCompFolder = makeFolder(folderName1);
             myCompOut.parentFolder = myOutCompFolder;
             var myMasterCompFolder = makeFolder(folderName2);
@@ -93,21 +93,8 @@ function prebalovator(compSelection, folderName1, folderName2) {
 
 slateOvator_part04();
 
+//-----------------------------------used methods
 /*
-var mySolid = projItem.layers.addSolid([1.0,1.0,0], "my square", 50, 50, 1);
-var regex = /slate_\(v\d{6}\)/;
-var testnameOne = "slate_(v240104)_02";
-var testnameTwo = "my square";
-//var slateSearch = regex.test(myStr);
-var mySolid = projItem.layer(testnameTwo);
-var myComp = mySelection
-//alert(projItem.layers[1].name);
-
-//alert([mySolid.width, mySolid.height]);
-*/
-
-/*
-
 Project.selection
 var mySelection = app.project.selection   //[]
 var myProjectItems = app.project.items;

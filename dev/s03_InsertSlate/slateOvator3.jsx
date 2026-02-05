a41740c3fcacbef393e95ecb5e607a70dd881d65
JanSvatuska <jan.svatuska@email.cz>
2026-02-05 21:05:13 +0100

    slateOvator3 insertSlate

--- dev/s03_InsertSlate/slateOvator3.jsx
+++ dev/s03_InsertSlate/slateOvator3.jsx
@@ -1,26 +1,48 @@
 // slateOvator3 insertSlate
-// 231211
+// slateOvator_part3_slateDistribution
+// v05
+// 231212
+
 
 app.beginUndoGroup("slate0vator3");
 
-var theComp = app.project.activeItem;
-
-//var slateName = "slate_(v231129)";
-var regex = /slate_\(v\d{6}\)$/;
-
-for (var i = 1; i <= app.project.numItems; i++) {
-    var myStr = app.project.item(i).name;
-    var slateSearch = regex.test(myStr);
-    
-    //if (app.project.item(i) instanceof CompItem && app.project.item(i).name == slateName) {
-    if (app.project.item(i) instanceof CompItem && slateSearch) {
-
-    var slate = app.project.item(i);
-    var newSlate = slate.duplicate();
-        theComp.layers.add(newSlate);
-        theComp.displayStartTime = -1;
-        //break;  //  verze s regexem jinak cykli
-    }
-}
+slateOvator3();
 
 app.endUndoGroup();
+
+function slateOvator3() {
+
+var selected = app.project.selection;
+
+    if (selected.length == 0) {
+        alert("Select a composition");
+    } else {
+        placeMultipleSlate(selected);
+    }
+}
+//  menime ve vyberu
+function placeMultipleSlate(compSelection) {
+    for (var j = 0; j < compSelection.length; j++) {
+        placeTheSlate(compSelection[j]);
+    }
+}
+//  vkladame kopii slatu do kompozice
+function placeTheSlate(theComp) {
+    
+    var regex = /slate_\(v\d{6}\)$/;
+    var projItem = app.project.item;
+
+    for (var i = 1; i <= app.project.numItems; i++) {
+        var myStr = app.project.item(i).name;
+        var slateSearch = regex.test(myStr);
+        
+        if (app.project.item(i) instanceof CompItem && slateSearch) {
+
+        var slate = app.project.item(i);
+        var newSlate = slate.duplicate();
+            theComp.layers.add(newSlate);
+            theComp.displayStartTime = -1;
+            //break;  //  verze s regexem jinak cykli
+        }
+    }
+}

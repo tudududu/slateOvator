cd0c8debc6c9a152c5bdbbf65d9b91408a66b65b
JanSvatuska <jan.svatuska@email.cz>
2026-02-05 21:03:56 +0100

    slateOvator3 insertSlate

--- dev/s03_InsertSlate/slateOvator3.jsx
+++ dev/s03_InsertSlate/slateOvator3.jsx
@@ -1,17 +1,25 @@
 // slateOvator3 insertSlate
-// 231210
+// 231211
 
 app.beginUndoGroup("slate0vator3");
 
 var theComp = app.project.activeItem;
-var regex = /v\d{3}/;
+
+//var slateName = "slate_(v231129)";
+var regex = /slate_\(v\d{6}\)$/;
 
 for (var i = 1; i <= app.project.numItems; i++) {
-    if (app.project.item(i) instanceof CompItem && app.project.item(i).name == "slate_(v231129)") {
+    var myStr = app.project.item(i).name;
+    var slateSearch = regex.test(myStr);
+    
+    //if (app.project.item(i) instanceof CompItem && app.project.item(i).name == slateName) {
+    if (app.project.item(i) instanceof CompItem && slateSearch) {
+
     var slate = app.project.item(i);
     var newSlate = slate.duplicate();
         theComp.layers.add(newSlate);
         theComp.displayStartTime = -1;
+        //break;  //  verze s regexem jinak cykli
     }
 }
 

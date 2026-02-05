e58acc753e1f86f706d81c9cf568c1d72018799c
JanSvatuska <jan.svatuska@email.cz>
2026-02-05 23:29:42 +0100

    slate0vator4 v01 duplicator_inRoot 03

--- dev/s04_completeAuto/duplicator_v01_inRoot/slate0vator4_v01.jsx
+++ dev/s04_completeAuto/duplicator_v01_inRoot/slate0vator4_v01.jsx
@@ -2,63 +2,98 @@
 // 240114
 
 //  The FinalCountdown
-//  240114_02a
+//  240114_03a
 //  out0vator
 
 
 //-----------------------------------
-app.beginUndoGroup("finalCountdown");
-//var projItems = app.project.items;
 
-var myCompMaster = app.project.activeItem;
-var outFolderName = "compsOut";
-        //var compFolder = app.project.items.addFolder(outFolderName);
-//criar "out" folder se ja nao existe
-    var testArr = [];
-for (var i = 1; i <= app.project.numItems; i++){
-    if (app.project.item(i) instanceof FolderItem && app.project.item(i).name == outFolderName) {
-        testArr.push(true);
+function slateOvator_part04() {
+app.beginUndoGroup("Prebal0vator");
+    var selected = app.project.selection;
+    //var regex = slateRegex();
+    var outFolderName = "compsOut";
+
+    if (selected.length == 0) {
+        alert("Select a composition");
+    } else {
+        placeSlateMultiComp(selected, outFolderName);
     }
-}
-    alert("numero dos outF: " + testArr.length);
-    if (testArr.length == 0) {
-        var compFolder = app.project.items.addFolder(outFolderName);
-    }
-
-var myCompMasterDur = myCompMaster.duration;
-var myCompOut = myCompMaster.duplicate();
-    myCompOut.duration = myCompMasterDur + 1;
-    myCompOut.displayStartTime = -1;
-
-var myCompOutLayers = myCompOut.layers;
-//alert(myCompOutLayers.length);
-//  delete all layer in myCompOut
-for (var i = myCompOutLayers.length; i >= 1; i--) {
-    var curLayer = myCompOutLayers[i];
-    curLayer.remove();   
-}
-    myCompOutLayers.add(myCompMaster);
-var myCompMasterLayerStartTime = myCompOut.layer(1).startTime;
-    myCompOut.layer(1).startTime = 1;
-    myCompOut.parentFolder = compFolder;
-
-//create a new FolderItem in project, with name "compsOut"
-//move myCompOut into new folder by setting
-//comp Item's parentFolder to "compsOut" folder
-//for (var i = 1; i <= app.project.numItems; i++) {
-  //if (app.project.item(i) instanceof CompItem) {
-    //app.project.item(i).parentFolder = compFolder;
-  //}
-//}
-
-
-
 app.endUndoGroup();
 
+//var projItems = app.project.items;
+//var myCompMaster = app.project.activeItem;
 
+function placeSlateMultiComp(compSelection, folderName) {
+        for (var j = 0; j < compSelection.length; j++) {
+            if (compSelection[j] instanceof CompItem) {
+            finalCountdown(compSelection[j], folderName);
+            }
+        }
+    }
+
+//criar "out" folder se ja nao existe
+    function makeFolder(folderName) {
+        var testArr = [];
+        for (var i = 1; i <= app.project.numItems; i++){
+            if (app.project.item(i) instanceof FolderItem && app.project.item(i).name == folderName) {
+                testArr.push(true);
+            }
+        }
+            //alert("numero dos outF: " + testArr.length);
+            var folderObj;
+            if (testArr.length == 0) {
+    //create a new FolderItem in project, with name "compsOut"
+                app.project.items.addFolder(folderName);
+            }
+            for (var i = 1; i <= app.project.numItems; i++){
+            if (app.project.item(i) instanceof FolderItem && app.project.item(i).name == folderName) {
+                folderObj = app.project.item(i);
+            }
+        }
+            return folderObj;
+        }
+
+    function finalCountdown(myCompMaster, folderName) {
+        var myCompMasterDur = myCompMaster.duration;
+        var myCompOut = myCompMaster.duplicate();
+            myCompOut.duration = myCompMasterDur + 1;
+            myCompOut.displayStartTime = -1;
+
+        var myCompOutLayers = myCompOut.layers;
+
+        //  delete layer in myCompOut
+        for (var i = myCompOutLayers.length; i >= 1; i--) {
+            var curLayer = myCompOutLayers[i];
+            curLayer.remove();
+        }
+
+            myCompOutLayers.add(myCompMaster);
+        var myCompMasterLayerStartTime = myCompOut.layer(1).startTime;
+            myCompOut.layer(1).startTime = 1;
+    //move myCompOut into new folder by setting
+    //comp Item's parentFolder to "compsOut" folder
+            var compFolder = makeFolder(folderName);
+            myCompOut.parentFolder = compFolder;
+    }
+}
+
+slateOvator_part04();
 
 /*
+var mySolid = projItem.layers.addSolid([1.0,1.0,0], "my square", 50, 50, 1);
+var regex = /slate_\(v\d{6}\)/;
+var testnameOne = "slate_(v240104)_02";
+var testnameTwo = "my square";
+//var slateSearch = regex.test(myStr);
+var mySolid = projItem.layer(testnameTwo);
+var myComp = mySelection
+//alert(projItem.layers[1].name);
 
+//alert([mySolid.width, mySolid.height]);
+*/
+
+/*
 
 Project.selection
 var mySelection = app.project.selection   //[]
@@ -81,22 +116,7 @@ app.project.item(index).layer(index).startTime
 myCompOut.displayStartTime = -1;
 myCompOut.parentFolder = compFolder;
 
-*/
 
-
-/*
-//var mySolid = projItem.layers.addSolid([1.0,1.0,0], "my square", 50, 50, 1);
-var regex = /slate_\(v\d{6}\)/;
-var testnameOne = "slate_(v240104)_02";
-var testnameTwo = "my square";
-//var slateSearch = regex.test(myStr);
-var mySolid = projItem.layer(testnameTwo);
-var myComp = mySelection
-//alert(projItem.layers[1].name);
-
-//alert([mySolid.width, mySolid.height]);
-*/
-/*
 var myScale = mySolid.scale;
     myScale.setValue([80, 80]);
 

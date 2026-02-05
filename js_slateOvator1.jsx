73e2b95dd1f3a65fe9797cb1f75b5fa8c532ee3e
JanSvatuska <jan.svatuska@email.cz>
2026-02-05 13:07:24 +0100

    slateOvator1 changeField: Operator

---
+++ js_slateOvator1.jsx
@@ -0,0 +1,63 @@
+(function (thisObj) {
+    
+    newPanel(thisObj);
+
+    function newPanel(thisObj) {
+        var win = (thisObj instanceof Panel) ? thisObj 
+        : new Window('palette', 'slateOvator', undefined);
+        //  win.preferredSize = [350, 300];
+
+        var groupOne = win.add('group');
+            groupOne.orientation = 'row';
+            groupOne.alignChildren = 'fill';
+        
+        //  label
+        var label = groupOne.add('statictext', undefined, 'operator: ');
+        //  input text    
+        var startTimeInput = groupOne.add('edittext', undefined, 'yourName');
+            startTimeInput.characters = 10;
+        
+        //  apply Button
+        var applyBtn = groupOne.add('button', undefined, 'Apply', {name: "ok"});
+        
+        // --- Action ---
+            applyBtn.onClick = function () {
+            slateOvator(startTimeInput.text);
+            //startTimeInput.active = true; // ---pokus o Enter minsto button click
+        }
+
+        // --- ACTIONS ---
+        win.onResizing = win.onResize = function () {
+            this.layout.resize();
+        };
+        win instanceof Window
+            ? (win.center(), win.show()) : (win.layout.layout(true), win.layout.resize());
+
+    }
+
+function slateOvator(newTextInput) {
+    app.beginUndoGroup("test");
+    var newText = newTextInput;
+    var selected = app.project.selection; //array
+
+    if (selected.length == 0) {
+        alert("Select a composition");
+    } else {
+        slateOvatorEngine(selected, newText);
+    }
+    
+    app.endUndoGroup();
+    
+    function slateOvatorEngine(arr, newText) {
+        for (var j = 0; j < arr.length; j++) {
+            var layerArr = arr[j].layers;
+            for (var i = 1; i <= layerArr.length; i++) {
+                if (layerArr[i].name == "Operator") {
+                    layerArr[i].text.sourceText.setValue(newText);
+                }
+            }
+        }
+    }
+}
+
+})(this);

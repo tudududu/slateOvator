84f6446af5da55aa04fb669eb4790e9503f04ccf
JanSvatuska <jan.svatuska@email.cz>
2026-02-05 23:26:50 +0100

    slate0vator4 v01 duplicator_inRoot 02

--- dev/s04_completeAuto/duplicator_v01_inRoot/slate0vator4_v01.jsx
+++ dev/s04_completeAuto/duplicator_v01_inRoot/slate0vator4_v01.jsx
@@ -2,16 +2,29 @@
 // 240114
 
 //  The FinalCountdown
-//  240114_01
+//  240114_02a
 //  out0vator
+
 
 //-----------------------------------
 app.beginUndoGroup("finalCountdown");
+//var projItems = app.project.items;
 
 var myCompMaster = app.project.activeItem;
-//var myCompWidth = myCompMaster.width;
-//var myCompHeight = myCompMaster.height;
-var compFolder = app.project.items.addFolder("compsOut");
+var outFolderName = "compsOut";
+        //var compFolder = app.project.items.addFolder(outFolderName);
+//criar "out" folder se ja nao existe
+    var testArr = [];
+for (var i = 1; i <= app.project.numItems; i++){
+    if (app.project.item(i) instanceof FolderItem && app.project.item(i).name == outFolderName) {
+        testArr.push(true);
+    }
+}
+    alert("numero dos outF: " + testArr.length);
+    if (testArr.length == 0) {
+        var compFolder = app.project.items.addFolder(outFolderName);
+    }
+
 var myCompMasterDur = myCompMaster.duration;
 var myCompOut = myCompMaster.duplicate();
     myCompOut.duration = myCompMasterDur + 1;
@@ -32,34 +45,43 @@ var myCompMasterLayerStartTime = myCompOut.layer(1).startTime;
 //create a new FolderItem in project, with name "compsOut"
 //move myCompOut into new folder by setting
 //comp Item's parentFolder to "compsOut" folder
-// for (var i = 1; i <= app.project.numItems; i++) {
-//   if (app.project.item(i) instanceof CompItem) {
-//     app.project.item(i).parentFolder = compFolder;
-//   }
-// }
+//for (var i = 1; i <= app.project.numItems; i++) {
+  //if (app.project.item(i) instanceof CompItem) {
+    //app.project.item(i).parentFolder = compFolder;
+  //}
+//}
+
+
 
 app.endUndoGroup();
 
-// Project.selection
-// var mySelection = app.project.selection   //[]
-// var myProjectItems = app.project.items;
-// var myActiveItem = app.project.activeItem;
 
-// Item object
-// app.project.item(index)
-// app.project.items[index]
 
-// ItemCollection.addComp()
-// app.project.items.addComp(name, width, height, pixelAspect, duration, frameRate)
+/*
 
-// CompItem.duplicate()
-// app.project.item(index).duplicate()
 
-// Layer.startTime
-// app.project.item(index).layer(index).startTime
+Project.selection
+var mySelection = app.project.selection   //[]
+var myProjectItems = app.project.items;
+var myActiveItem = app.project.activeItem;
 
-// myCompOut.displayStartTime = -1;
-// myCompOut.parentFolder = compFolder;
+Item object
+app.project.item(index)
+app.project.items[index]
+
+ItemCollection.addComp()
+app.project.items.addComp(name, width, height, pixelAspect, duration, frameRate)
+
+CompItem.duplicate()
+app.project.item(index).duplicate()
+
+Layer.startTime
+app.project.item(index).layer(index).startTime
+
+myCompOut.displayStartTime = -1;
+myCompOut.parentFolder = compFolder;
+
+*/
 
 
 /*
@@ -74,7 +96,6 @@ var myComp = mySelection
 
 //alert([mySolid.width, mySolid.height]);
 */
-
 /*
 var myScale = mySolid.scale;
     myScale.setValue([80, 80]);

4246fd454699fd5037a35dbb0732c8d3e5933e07
JanSvatuska <jan.svatuska@email.cz>
2026-02-05 21:03:25 +0100

    slateOvator3 insertSlate

--- dev/s03_InsertSlate/slateOvator3.jsx
+++ dev/s03_InsertSlate/slateOvator3.jsx
@@ -1,15 +1,17 @@
 // slateOvator3 insertSlate
-// 231208
+// 231210
 
 app.beginUndoGroup("slate0vator3");
 
 var theComp = app.project.activeItem;
+var regex = /v\d{3}/;
 
-var newComp = theComp.duplicate();
 for (var i = 1; i <= app.project.numItems; i++) {
-    if (app.project.item(i).name == "previewComp") {
-    var previewComp = app.project.item(i);
-        previewComp.layers.add(newComp);
+    if (app.project.item(i) instanceof CompItem && app.project.item(i).name == "slate_(v231129)") {
+    var slate = app.project.item(i);
+    var newSlate = slate.duplicate();
+        theComp.layers.add(newSlate);
+        theComp.displayStartTime = -1;
     }
 }
 

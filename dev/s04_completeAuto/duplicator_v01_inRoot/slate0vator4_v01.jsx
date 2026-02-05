c0336d75b10fa7d3561db0f0ff0e1a25b06ad373
JanSvatuska <jan.svatuska@email.cz>
2026-02-05 23:47:28 +0100

    slate0vator4 v01 duplicator_inRoot 05b

--- dev/s04_completeAuto/duplicator_v01_inRoot/slate0vator4_v01.jsx
+++ dev/s04_completeAuto/duplicator_v01_inRoot/slate0vator4_v01.jsx
@@ -1,10 +1,9 @@
 // slate0vator4 v01 duplicator_inRoot
-// 240127
+// 240129
 
 //  The FinalCountdown
-//  240119_05a
+//  240127_05b
 //  out0vator prebal0vator
-
 
 function slateOvator_part04() {
 app.beginUndoGroup("Prebal0vator");
@@ -16,19 +15,19 @@ app.beginUndoGroup("Prebal0vator");
     if (selected.length == 0) {
         alert("Select a composition");
     } else {
-        prebalovator(selected, outFolderName, masterFolderName);
+        prebalVyber(selected, outFolderName, masterFolderName);
     }
 app.endUndoGroup();
 
-function prebalovator(compSelection, folderName1, folderName2) {
+function prebalVyber(compSelection, folderNameOut, folderNameMaster) {
         for (var j = 0; j < compSelection.length; j++) {
             if (compSelection[j] instanceof CompItem) {
-            finalCountdown(compSelection[j], folderName1, folderName2);
+            prebalovac(compSelection[j], folderNameOut, folderNameMaster);
             }
         }
     }
 
-//criar "out" folder se ja nao existe
+    //  criar "out" folder se ja nao existe
     function makeFolder(folderName) {
         //  scan proj if folder exist
         //  pushes 'true' into testArr in case the folder exists
@@ -41,36 +40,32 @@ function prebalovator(compSelection, folderName1, folderName2) {
         }
         //  if the folder not exist
         //  create a new FolderItem in project
-            var folderObj;
             if (testArr.length == 0) {
                 app.project.items.addFolder(folderName);
                 }
         //  find folderObj in the proj and put it to var
-            for (var i = 1; i <= app.project.numItems; i++){
-            if (app.project.item(i).name == folderName 
+        //  nutne, v pripade, ze uz existuje
+        //  jen vlozit do promenne nefunguje
+            var folderObj;
+           for (var i = 1; i <= app.project.numItems; i++){
+                if (app.project.item(i).name == folderName 
                 && app.project.item(i) instanceof FolderItem) {
                 folderObj = app.project.item(i);
+                }
             }
-        }
             return folderObj;
-        }
+    }
 
-    function finalCountdown(myCompMaster, folderName1, folderName2) {
+    function prebalovac(myCompMaster, folderNameOut, folderNameMaster) {
         
-        //  a criar o comp para o export
+        //  a criar o 'outComp'
         var myCompMasterDur = myCompMaster.duration;
         var myCompOut = myCompMaster.duplicate();
             myCompOut.duration = myCompMasterDur + 1;
             myCompOut.displayStartTime = -1;
+        
         //  naming: 
-        //  passar o nome original para o compOut
-        //  usar um arr para o troco
-        var compNameArr = [];
-        compNameArr.push(myCompMaster.name);
-        compNameArr.push(myCompOut.name);
-        var masterAppendix = "_master";
-        myCompMaster.name = compNameArr[0] + masterAppendix;
-        myCompOut.name = compNameArr[0];
+        naming(myCompMaster, myCompOut);
         
         //  delete layers in myCompOut
         var myCompOutLayers = myCompOut.layers;
@@ -84,45 +79,23 @@ function prebalovator(compSelection, folderName1, folderName2) {
             myCompOut.layer(1).startTime = 1;
         //  move myCompOut into new folder by setting
         //  comp Item's parentFolder to "compsOut" folder
-            var myOutCompFolder = makeFolder(folderName1);
+            var myOutCompFolder = makeFolder(folderNameOut);
             myCompOut.parentFolder = myOutCompFolder;
-            var myMasterCompFolder = makeFolder(folderName2);
+            var myMasterCompFolder = makeFolder(folderNameMaster);
             myCompMaster.parentFolder = myMasterCompFolder;
     }
+
+    function naming(myCompMaster, myCompOut) {
+    //  k puvodnimu jmenu pridavame priponu a duplikat prebira puvodni nazev
+    //  passar o nome original para o compOut
+    //  usar um arr para o troco
+        var compNameArr = [];
+        compNameArr.push(myCompMaster.name);
+        compNameArr.push(myCompOut.name);
+        var masterAppendix = "_master";
+        myCompMaster.name = compNameArr[0] + masterAppendix;
+        myCompOut.name = compNameArr[0];
+        }
 }
 
 slateOvator_part04();
-
-//-----------------------------------used methods
-/*
-Project.selection
-var mySelection = app.project.selection   //[]
-var myProjectItems = app.project.items;
-var myActiveItem = app.project.activeItem;
-
-Item object
-app.project.item(index)
-app.project.items[index]
-
-ItemCollection.addComp()
-app.project.items.addComp(name, width, height, pixelAspect, duration, frameRate)
-
-CompItem.duplicate()
-app.project.item(index).duplicate()
-
-Layer.startTime
-app.project.item(index).layer(index).startTime
-
-myCompOut.displayStartTime = -1;
-myCompOut.parentFolder = compFolder;
-
-
-var myScale = mySolid.scale;
-    myScale.setValue([80, 80]);
-
-var myPosition = mySolid.property("position");
-    myPosition.setValue([180,300]);
-
-var myOpacity = mySolid.opacity;
-    myOpacity.setValue(30);
-*/

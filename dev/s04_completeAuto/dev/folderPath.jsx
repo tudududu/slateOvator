603897136889ecac17401481f32c36542e6213f9
JanSvatuska <jan.svatuska@email.cz>
2026-02-06 11:16:10 +0100

    folderPath 240122 03
    get AE project path in folder structure

---
+++ dev/s04_completeAuto/dev/folderPath.jsx
@@ -0,0 +1,41 @@
+// folderPath
+// 240122
+// pokus dostat cestu ve strukture slozek
+
+/*
+var myComp = app.project.activeItem;
+var myCompName = myComp.name;
+var myCompFolder = myComp.parentFolder;
+var myCompFolderName = myCompFolder.name;
+*/
+
+var myFolder = app.project.activeItem;
+var myFolderParentL1 = myFolder.parentFolder;
+var mFPL1 = folderNameFNC(myFolderParentL1);
+
+var myFolderParentL2 = myFolderParentL1.parentFolder;
+var mFPL2 = folderNameFNC(myFolderParentL2);
+
+var myFolderParentL3 = myFolderParentL2.parentFolder;
+var mFPL3 = folderNameFNC(myFolderParentL3);
+
+var myFolderParentL4 = myFolderParentL3.parentFolder;
+var mFPL4 = folderNameFNC(myFolderParentL4);
+
+var myFolderParentL5 = myFolderParentL4.parentFolder;
+var mFPL5 = folderNameFNC(myFolderParentL5);
+
+function folderNameFNC(item) {
+    var result;
+    if (item != null && item instanceof FolderItem) {
+        result = item.name;
+    } else {
+        result = null;
+    }
+    return result;
+}
+
+var pathArr = [mFPL1, mFPL2, mFPL3, mFPL5, null ,mFPL4];
+var nullArr = [mFPL5, null];
+alert(pathArr);
+alert(mFPL5);

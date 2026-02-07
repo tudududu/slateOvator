c8b90641b7fe187faf273423b5701c4d875aeba0
JanSvatuska <jan.svatuska@email.cz>
2026-02-07 18:12:58 +0100

    test_set_text.sourceText.value 231204 02

--- dev/test/test_set_text.sourceText.value.jsx
+++ dev/test/test_set_text.sourceText.value.jsx
@@ -6,5 +6,5 @@ var selected = app.project.selection; //array
 //var comp = app.project.activeItem;
 var layers = comp.selectedLayers;
 //alert(selected[0].name);
-alert(layers);
+alert(layers[1].name);
 app.endUndoGroup();

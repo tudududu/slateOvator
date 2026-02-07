8b6de46ad8b4a8949215134ccbf8269277c780a3
JanSvatuska <jan.svatuska@email.cz>
2026-02-07 17:44:35 +0100

    test_autoScalingSlate 06 240209

--- dev/test/test_autoScalingSlate.jsx
+++ dev/test/test_autoScalingSlate.jsx
@@ -17,37 +17,45 @@ var myLayerSize = [mySolid.width, mySolid.height];
 var myCompSize = [projItem.width, projItem.height];
 //alert([myCompSize[0], myLayerSize[0]]);
 
+var compAspect = (myCompSize[0] / myCompSize[1]).toFixed(2);
+//alert([compAspect]);
 var myScale = mySolid.scale;
-var hdEightyX = 1920 * 0.81;    // 80% HDx
+var hd80X = 1920 * 0.81;    // 80% HDx
 var scaleX;
 var scaleX_00 = 100;
-var scaleX_01 = myCompSize[0] / hdEightyX * 100;    // zmensujem od 80% HD
+var scaleX_01 = myCompSize[0] / hd80X * 100;    // zmensujem od 80% HD
 var scaleX_02 = myCompSize[0] / 1920 * 100; // zvetsujem nad 1920
 
-if (myCompSize[0] >= hdEightyX && myCompSize[0] <= 1920) {
+if (myCompSize[0] >= hd80X && myCompSize[0] <= 1920) {
     scaleX = scaleX_00;
-} else if (myCompSize[0] < hdEightyX) {
+} else if (myCompSize[0] < hd80X) {
     scaleX = scaleX_01;
 } else if (myCompSize[0] > 1920) {
     scaleX = scaleX_02;
 }
 
-var hdEightyY = 1080 * 0.81;    // 80% HDx
+var hd80Y = 1080 * 0.81;    // 80% HDx
 var scaleY;
 var scaleY_00 = 100;
-var scaleY_01 = myCompSize[1] / hdEightyY * 100;    // zmensujem od 80% HD
+var scaleY_01 = myCompSize[1] / hd80Y * 100;    // zmensujem od 80% HD
 var scaleY_02 = myCompSize[1] / 1080 * 100; // zvetsujem nad 1920
 
-if (myCompSize[1] >= hdEightyY && myCompSize[1] <= 1080) {
+if (myCompSize[1] >= hd80Y && myCompSize[1] <= 1080) {
     scaleY = scaleY_00;
-} else if (myCompSize[1] < hdEightyY) {
+} else if (myCompSize[1] < hd80Y) {
     scaleY = scaleY_01;
 } else if (myCompSize[1] > 1080) {
     scaleY = scaleY_02;
 }
-//alert([scaleX, scaleY]);
-//if (scaleX >)
-    myScale.setValue([scaleX, scaleY]);
+alert("scaleX: " + scaleX + ", scaleY: " + scaleY);
+
+if (compAspect <= 1.78) {
+    scale = scaleX;
+} else {
+    scale = scaleY;
+}
+
+    myScale.setValue([scale, scale]);
 
 
 //var mySolidResize = projItem.layer(testnameOne);

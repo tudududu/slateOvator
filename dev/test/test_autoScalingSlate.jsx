e662342c00e54468462b6701a2a8dc103e562f8b
JanSvatuska <jan.svatuska@email.cz>
2026-02-07 17:45:06 +0100

    test_autoScalingSlate 07 240209

--- dev/test/test_autoScalingSlate.jsx
+++ dev/test/test_autoScalingSlate.jsx
@@ -5,8 +5,6 @@ app.beginUndoGroup("Create Square");
 
 var projItem = app.project.activeItem;
 
-//var mySolid = projItem.layers.addSolid([1.0,1.0,0], "my square", 50, 50, 1);
-
 var regex = /slate_\(v\d{6}\)/;
 var testnameOne = "slate_(v240104)_02";
 var testnameTwo = "my square";
@@ -15,27 +13,56 @@ var mySolid = projItem.layer(testnameOne);
 //alert(projItem.layers[1].name);
 var myLayerSize = [mySolid.width, mySolid.height];
 var myCompSize = [projItem.width, projItem.height];
-//alert([myCompSize[0], myLayerSize[0]]);
+//alert(myCompSize);
 
 var compAspect = (myCompSize[0] / myCompSize[1]).toFixed(2);
+
 //alert([compAspect]);
 var myScale = mySolid.scale;
-var hd80X = 1920 * 0.81;    // 80% HDx
-var scaleX;
+var hd80X = 1920 * 0.81;    // bottomMargin = 80% HDx
+var hd80Y = 1080 * 0.75;
+
+function scaleCondition(myCompSizeLocal, bottomMargin, topMargin) {
+    //alert(myCompSizeLocal);
+    var scaleResult;
+    var scale_00 = 100;
+    var scale_01 = myCompSizeLocal / bottomMargin * 100;    // zmensujem od 80% HD
+    var scale_02 = myCompSizeLocal / topMargin * 100; // zvetsujem nad 1920
+
+    if (myCompSizeLocal >= bottomMargin && myCompSizeLocal <= topMargin) {
+        scaleResult = scale_00;
+    } else if (myCompSizeLocal < bottomMargin) {
+        scaleResult = scale_01;
+    } else if (myCompSizeLocal > topMargin) {
+        scaleResult = scale_02;
+    }
+
+    return scaleResult;
+}
+
+var scaleX = scaleCondition(myCompSize[0], hd80X, 1920);
+var scaleY = scaleCondition(myCompSize[1], hd80Y, 1080);
+
+//  alert("scaleX: " + scaleX + ", scaleY: " + scaleY);
+
+    if (compAspect <= 1.78) {
+        scale = scaleX;
+    } else {
+        scale = scaleY;
+    }
+
+    myScale.setValue([scale, scale]);
+
+app.endUndoGroup();
+
+
+
+/*var scaleX;
 var scaleX_00 = 100;
 var scaleX_01 = myCompSize[0] / hd80X * 100;    // zmensujem od 80% HD
 var scaleX_02 = myCompSize[0] / 1920 * 100; // zvetsujem nad 1920
-
-if (myCompSize[0] >= hd80X && myCompSize[0] <= 1920) {
-    scaleX = scaleX_00;
-} else if (myCompSize[0] < hd80X) {
-    scaleX = scaleX_01;
-} else if (myCompSize[0] > 1920) {
-    scaleX = scaleX_02;
-}
-
-var hd80Y = 1080 * 0.81;    // 80% HDx
-var scaleY;
+*/
+/*var scaleY;
 var scaleY_00 = 100;
 var scaleY_01 = myCompSize[1] / hd80Y * 100;    // zmensujem od 80% HD
 var scaleY_02 = myCompSize[1] / 1080 * 100; // zvetsujem nad 1920
@@ -47,26 +74,4 @@ if (myCompSize[1] >= hd80Y && myCompSize[1] <= 1080) {
 } else if (myCompSize[1] > 1080) {
     scaleY = scaleY_02;
 }
-alert("scaleX: " + scaleX + ", scaleY: " + scaleY);
-
-if (compAspect <= 1.78) {
-    scale = scaleX;
-} else {
-    scale = scaleY;
-}
-
-    myScale.setValue([scale, scale]);
-
-
-//var mySolidResize = projItem.layer(testnameOne);
-//alert(mySolidResize.sourceRectAtTime(0, false).width);
-/*
-var myPosition = mySolid.property("position");
-    myPosition.setValue([180,300]);
-
-var myOpacity = mySolid.opacity;
-    myOpacity.setValue(30);
-    */
-app.endUndoGroup();
-
-//compWidth/1540*100
+*/

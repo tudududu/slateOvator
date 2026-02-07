5ff888d38aa08ca048cc5173f3c340febd48fd51
JanSvatuska <jan.svatuska@email.cz>
2026-02-07 18:13:26 +0100

    test_set_text.sourceText.value 231204 03

--- dev/test/test_set_text.sourceText.value.jsx
+++ dev/test/test_set_text.sourceText.value.jsx
@@ -4,7 +4,19 @@
 app.beginUndoGroup("test");
 var selected = app.project.selection; //array
 //var comp = app.project.activeItem;
-var layers = comp.selectedLayers;
+var layers = selected[0].layers;
 //alert(selected[0].name);
 alert(layers[1].name);
+/*
+var layerName;
+
+for (var i = 0; i < layers.legth; i++) {
+    if (layers[i].name == "Operator") {
+        //layers[i].SourceText = "Honza";
+        alert(layers[i].name);
+    }
+}
+
+alert(layerName);
+*/
 app.endUndoGroup();

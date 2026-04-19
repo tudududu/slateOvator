b63c1c8b03d61ad42d0c4785c5117ff788a8e2e8
JanSvatuska <jan.svatuska@email.cz>
2026-04-19 11:32:05 +0200

    test_UI_elementInColor 241114 v03
    small red square in the corner of the panel, mouseover effect,
    panel size adjusted, added panel title

--- dev/test/test_UI_elementInColor/test_UI_elementInColor.jsx
+++ dev/test/test_UI_elementInColor/test_UI_elementInColor.jsx
@@ -1,52 +1,33 @@
 //  After Effects script UI test - element in color
 //  241114
-//  v02
-//  small red square in the corner of the panel, mouseover effect
+//  v03
+//  small red square in the corner of the panel, mouseover effect, 
+//  panel size adjusted, added panel title
 
 (function (thisObj) {
     
     newPanel(thisObj);
 
     function newPanel(thisObj) {
+        var testSize = [350, 100]
         var win = (thisObj instanceof Panel) ? thisObj 
         : new Window('palette', 'slateOvator2', undefined);
-        //  win.preferredSize = [350, 300];
+            win.preferredSize = testSize;
 
         var groupOne = win.add('group');
             groupOne.orientation = 'column';
             groupOne.alignChildren = 'fill';
-        /*
-        //  input text    
-        var startTimeInput = groupOne.add('edittext', undefined, 'yourName');
-            startTimeInput.characters = 10;
-        */
-        //  label
-        var label = groupOne.add('statictext', undefined, 'Pass the compName to the slate');
+
+        //  --------panel05--------Insert slate into composition--------
+        var panel05 = groupOne.add('panel', undefined, "Insert slate");
+            panel05.orientation = 'column';
+            panel05.alignChildren = 'fill';
+            panel05.preferredSize = testSize;
         //  apply Button
-        var applyBtn = groupOne.add('button', undefined, 'Apply', {name: "ok"});
-        
-        // --- Customize Button Highlight Color ---
-        /* applyBtn.graphics.foregroundColor = applyBtn.graphics.newPen(
-            applyBtn.graphics.PenType.SOLID_COLOR, [1, 0, 0, 1], 1); // Red color */
-        
-        // --- Customize Button Highlight Color ---
-        /* applyBtn.onDraw = function () {
-            var g = applyBtn.graphics;
-            var brush = g.newBrush(g.BrushType.SOLID_COLOR, [1, 0, 0, 1]); // Red color
-            g.rectPath(0, 0, applyBtn.size[0], applyBtn.size[1]);
-            g.fillPath(brush);
-        }; */
+        var applyBtn = panel05.add('button', undefined, 'Complete insert');
         
         // --- Variables to track mouse state ---
         var isMouseOver = false;
-        // --- Customize Button Highlight Color ---
-        /* applyBtn.onDraw = function () {
-            var g = applyBtn.graphics;
-            var brush = g.newBrush(g.BrushType.SOLID_COLOR, isMouseOver ? [1, 0, 0, 1] : [0.2, 0.2, 0.2, 1]); // Red on hover, grey otherwise
-            g.rectPath(0, 0, applyBtn.size[0]/2, applyBtn.size[1]);
-            g.fillPath(brush);
-            g.drawString("Apply", g.font, g.newPen(g.PenType.SOLID_COLOR, [0, 0, 0, 1], 1), 10, 10); // Draw the button text
-        }; */
 
         // --- Customize Button Highlight Color ---
         applyBtn.onDraw = function () {
@@ -56,14 +37,7 @@
             g.rectPath(0, 0, 10, 10);
             g.fillPath(brush);
 
-            // Draw the button frame
-            // var pen = g.newPen(g.PenType.SOLID_COLOR, [0.9, 0.9, 0.9, 1], 1); // White color
-            // g.strokePath(pen);
-
-            // Draw the button text
-            // g.drawString("Apply", g.font, g.newPen(g.PenType.SOLID_COLOR, [0, 0, 0, 1], 1), 10, 10); // Black text
         };
-
 
         // --- Mouse event handlers ---
         applyBtn.addEventListener('mouseover', function () {
@@ -75,17 +49,6 @@
             isMouseOver = false;
             applyBtn.notify('onDraw');
         });
-
-        // // --- Mouse event handlers ---
-        // applyBtn.addEventListener('mouseover', function () {
-        //     isMouseOver = true;
-        //     applyBtn.notify('onDraw');
-        // });
-
-        // applyBtn.addEventListener('mouseout', function () {
-        //     isMouseOver = false;
-        //     applyBtn.notify('onDraw');
-        // });
 
         // --- Action ---
             applyBtn.onClick = function () {
@@ -100,6 +63,5 @@
             ? (win.center(), win.show()) : (win.layout.layout(true), win.layout.resize());
 
     }
-
 
 })(this);

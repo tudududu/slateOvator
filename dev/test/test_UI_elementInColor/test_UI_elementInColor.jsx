cd9c513843939a86b14c6b5a19d09d7ffec63205
JanSvatuska <jan.svatuska@email.cz>
2026-04-19 11:33:47 +0200

    test_UI_elementInColor 241114 v04
    button red on mouseover, big red banner above the button

--- dev/test/test_UI_elementInColor/test_UI_elementInColor.jsx
+++ dev/test/test_UI_elementInColor/test_UI_elementInColor.jsx
@@ -1,45 +1,49 @@
 //  After Effects script UI test - element in color
 //  241114
-//  v03
-//  small red square in the corner of the panel, mouseover effect, 
-//  panel size adjusted, added panel title
+//  v04
+//  button red on mouseover,
+//  big red banner above the button
 
 (function (thisObj) {
     
     newPanel(thisObj);
 
     function newPanel(thisObj) {
-        var testSize = [350, 100]
-        var win = (thisObj instanceof Panel) ? thisObj 
-        : new Window('palette', 'slateOvator2', undefined);
-            win.preferredSize = testSize;
+        var testSize = [350, 100];
+        var win = (thisObj instanceof Panel) ? thisObj : new Window('palette', 'slateOvator2', undefined);
+        win.preferredSize = testSize;
 
         var groupOne = win.add('group');
-            groupOne.orientation = 'column';
-            groupOne.alignChildren = 'fill';
+        groupOne.orientation = 'column';
+        groupOne.alignChildren = 'fill';
 
-        //  --------panel05--------Insert slate into composition--------
+        // Panel for inserting slate
         var panel05 = groupOne.add('panel', undefined, "Insert slate");
-            panel05.orientation = 'column';
-            panel05.alignChildren = 'fill';
-            panel05.preferredSize = testSize;
-        //  apply Button
+        panel05.orientation = 'column';
+        panel05.alignChildren = 'fill';
+        panel05.preferredSize = testSize;
+
+        // Draw colored element
+        var colorElement = panel05.add('panel', undefined);
+        colorElement.preferredSize = [100, 100]; // Size of the colored element
+        colorElement.graphics.backgroundColor = colorElement.graphics.newBrush(colorElement.graphics.BrushType.SOLID_COLOR, [1, 0, 0, 1]); // Red color
+
+        // Apply Button
         var applyBtn = panel05.add('button', undefined, 'Complete insert');
-        
+        applyBtn.preferredSize = [150, 40]; // Adjust size as needed
+
         // --- Variables to track mouse state ---
         var isMouseOver = false;
 
-        // --- Customize Button Highlight Color ---
+        // Customize Button Highlight Color
         applyBtn.onDraw = function () {
             var g = applyBtn.graphics;
             var brush = g.newBrush(g.BrushType.SOLID_COLOR, isMouseOver ? [1, 0, 0, 1] : [0.1, 0.1, 0.1, 1]); // Red on hover, grey otherwise
-            // g.rectPath(0, 0, applyBtn.size[0], applyBtn.size[1]);
-            g.rectPath(0, 0, 10, 10);
+            g.rectPath(0, 0, applyBtn.size[0], applyBtn.size[1]);
             g.fillPath(brush);
-
         };
 
-        // --- Mouse event handlers ---
+        // Mouse event handlers
         applyBtn.addEventListener('mouseover', function () {
             isMouseOver = true;
             applyBtn.notify('onDraw');
@@ -50,18 +54,16 @@
             applyBtn.notify('onDraw');
         });
 
-        // --- Action ---
-            applyBtn.onClick = function () {
+        // Action on button click
+        applyBtn.onClick = function () {
             slateOvator2();
-        }
+        };
 
-        // --- ACTIONS ---
+        // Resize actions
         win.onResizing = win.onResize = function () {
             this.layout.resize();
         };
-        win instanceof Window
-            ? (win.center(), win.show()) : (win.layout.layout(true), win.layout.resize());
-
+        win instanceof Window ? (win.center(), win.show()) : (win.layout.layout(true), win.layout.resize());
     }
 
 })(this);

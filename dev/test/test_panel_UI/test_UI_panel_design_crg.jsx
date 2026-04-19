2af6df09a1e5429f63fc0658f1228f59b75a4643
JanSvatuska <jan.svatuska@email.cz>
2026-04-19 10:25:09 +0200

    test_UI_panel_design 241012 type02
    start() + buildUI() + doMain() structure;

--- dev/test/test_panel_UI/test_UI_panel_design_crg.jsx
+++ dev/test/test_panel_UI/test_UI_panel_design_crg.jsx
@@ -1,62 +1,46 @@
-// test_UI_panel_design_crg
-// 241011
-// start() + buildUI() + doMain() structure; 
+// test_UI_panel_design type02 crg
+// 241012
+// start() + buildUI() + doMain() structure;
 
 (function (thisObj) {
-    //globals: ////
-    var vers = "x.x";
-    
+
 var win = (thisObj instanceof Panel) ? thisObj 
-        : new Window('palette', 'compsChanger (v' + vers + ')', undefined);
-var w = buildUI(thisObj);
-var probStr = "";
-/////////////
+        : new Window('palette', 'compsChanger' , undefined);
+var ui = buildUI(thisObj);
 
 start();
 
 function start() {
     if (app.project != null) {
-        if (w != null) {
-            w.show();
-        } else {
-            alert("w == null")
+        if (ui != null) {
+            ui.show();
         }
-    } else {
-        alert("app.project == null")
     }
+    return win;
 }
 
 function buildUI(thisObj) {
     if (win != null) {
-
+        
     win.widthLabel = win.add('statictext', undefined, 'Width:');
-    win.widthT = win.add('edittext', [89,177,166,199], '');
+    win.widthT = win.add('edittext', undefined, '');
     
-    win.okBtn = win.add('button', [89,177,166,199], 'OK', {name:'ok'});
+    win.okBtn = win.add('button', undefined, 'OK', {name:'ok'});
     
     win.okBtn.onClick = function () { 
         doMain(this.parent); // Calls doMain with the win object
         //this.parent.close(1); // Closes the win window
-        };
+    };
+
     }
     return win;
 }
+
 function doMain(theDialog) {
     app.beginUndoGroup("Change Selected Comps");
     
-    if (theDialog.widthT.text != "") {
-        if (isNaN(parseFloat(theDialog.widthT.text))) {
-            probStr = (probStr + "Not a number value for Width\r");
-            theDialog.widthT.text = "";//empty field if it is bad so we don't try anymore
-        } else {
-        //    oldWidth = item.width;
-        //    newWidth = Math.floor(parseFloat(theDialog.widthT.text));
-        //    if ( (newWidth > 30000) || (newWidth < 4) ) {
-        //        probStr = (probStr + "Value out of range for Width\r");
-            probStr = (probStr + "Value is OK\r");
-            theDialog.widthT.text = "";//empty field if it is bad so we don't try anymore
-            }
-        }
+    alert(theDialog.widthT.text);
+
     app.endUndoGroup();
     }
 
